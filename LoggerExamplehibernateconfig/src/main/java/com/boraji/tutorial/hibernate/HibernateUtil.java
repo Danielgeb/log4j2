5c71a74ac6b6a48f1365d4825db94e6241b78d77
DanielGeb <dfgebreh@gmail.com>
2021-02-21 15:24:44 -0500

    log4j2 example

---
+++ LoggerExamplehibernateconfig/src/main/java/com/boraji/tutorial/hibernate/HibernateUtil.java
@@ -0,0 +1,66 @@
+package com.boraji.tutorial.hibernate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+
+import com.boraji.tutorial.hibernate.entity.Customer;
+
+public class HibernateUtil {
+
+   private static final Logger logger = LogManager.getLogger(HibernateUtil.class);
+
+   private static StandardServiceRegistry registry;
+   private static SessionFactory sessionFactory;
+
+   public static SessionFactory getSessionFactory() {
+      if (sessionFactory == null) {
+         try {
+            StandardServiceRegistryBuilder registryBuilder = 
+                  new StandardServiceRegistryBuilder();
+            
+            Map<String, String> settings = new HashMap<>();
+            settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            settings.put(Environment.URL, "jdbc:mysql://localhost:3306/plantplaces");
+            settings.put(Environment.USER, "root");
+            settings.put(Environment.PASS, "root");
+            settings.put(Environment.HBM2DDL_AUTO, "update");
+
+            registryBuilder.applySettings(settings);
+
+            registry = registryBuilder.build();
+
+            logger.info("Hibernate Registry builder created.");
+            
+            MetadataSources sources = new MetadataSources(registry).addAnnotatedClass(Customer.class);
+
+            Metadata metadata = sources.getMetadataBuilder().build();
+
+            sessionFactory = metadata.getSessionFactoryBuilder().build();
+            
+            logger.info("SessionFactory created.");
+            
+         } catch (Exception e) {
+            logger.info("SessionFactory creation failed");
+            if (registry != null) {
+               StandardServiceRegistryBuilder.destroy(registry);
+            }
+         }
+      }
+      return sessionFactory;
+   }
+
+   public static void shutdown() {
+      if (registry != null) {
+         StandardServiceRegistryBuilder.destroy(registry);
+      }
+   }
+}
---
+++ LoggerExamplehibernateconfig/src/main/java/com/boraji/tutorial/hibernate/MainApp.java
@@ -0,0 +1,50 @@
+package com.boraji.tutorial.hibernate;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.boraji.tutorial.hibernate.entity.Customer;
+
+/**
+ * @author imssbora
+ */
+public class MainApp {
+
+   private static final Logger logger = LogManager.getLogger(MainApp.class);
+
+   public static void main(String[] args) {
+      Session session = null;
+      Transaction transaction = null;
+      try {
+    	  
+         session = HibernateUtil.getSessionFactory().openSession();
+         transaction = session.beginTransaction();
+         if(!transaction.isActive())  
+         transaction.begin();
+
+         Customer customer = new Customer();
+         customer.setName("Joe");
+         session.persist(customer);
+
+         transaction.commit();
+
+         logger.info("Customer saved successfully...");
+
+      } catch (Exception e) {
+         if (transaction != null) {
+            transaction.rollback();
+         }
+         
+
+         logger.error("Failed to save customer..." + e);
+      } finally {
+         if (session != null) {
+            session.close();
+         }
+      }
+
+      HibernateUtil.shutdown();
+   }
+}
